6e7ee8f59e1b9795aa9d1a38c9cf1b047c20ceea
Franz Holzinger <franz.holzinger@festwein.de>
2022-01-20 16:46:23 +0100

    * bugfix issue #1: rename namespace XClass ==> Xclass

--- Classes/Xclass/Schema/SchemaMigrator.php
+++ Classes/Xclass/Schema/SchemaMigrator.php
@@ -15,7 +15,7 @@ declare(strict_types=1);
  * The TYPO3 project - inspiring people to share!
  */
 
-namespace JambageCom\AddonsEm\XClass\Schema;
+namespace JambageCom\AddonsEm\Xclass\Schema;
 
 
 use Doctrine\DBAL\Exception as DBALException;
--- ext_localconf.php
+++ ext_localconf.php
@@ -2,7 +2,7 @@
 defined('TYPO3_MODE') || die('Access denied.');
 
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][TYPO3\CMS\Core\Database\Schema\SchemaMigrator::class] = [
-   'className' => JambageCom\AddonsEm\XClass\Schema\SchemaMigrator::class
+   'className' => JambageCom\AddonsEm\Xclass\Schema\SchemaMigrator::class
 ];
 
 
