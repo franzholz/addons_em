b2a341bed951144d81616bdfdf6735917ca3a234
Franz Holzinger <franz.holzinger@festwein.de>
2023-04-15 19:38:05 +0200

    compatible with TYPO3 12.3

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,5 @@
 <?php
-defined('TYPO3_MODE') || die('Access denied.');
+defined('TYPO3') || die('Access denied.');
 
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][TYPO3\CMS\Core\Database\Schema\SchemaMigrator::class] = [
    'className' => JambageCom\AddonsEm\Xclass\Schema\SchemaMigrator::class
